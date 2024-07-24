bc51a434f822a9778666f3db80d691eed939f8dc
bryChen19 <bryanchenontario@gmail.com>
2024-07-24 11:44:38 -0400

    express edit1

--- server/express.js
+++ server/express.js
@@ -18,7 +18,8 @@ const CURRENT_WORKING_DIR = process.cwd()
  res.status(200).send(Template()) 
  })*/
  
-app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')))
+app.use(express.static(path.join(CURRENT_WORKING_DIR, "dist/app")));
+app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/', userRoutes)
