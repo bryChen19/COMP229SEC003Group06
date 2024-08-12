664008faaff3d7f413423bcbaff675be04773b50
bryChen19 <bryanchenontario@gmail.com>
2024-08-12 00:29:47 -0400

    Reapply "users should be fixed"
    This reverts commit b9500acb582bf7c4b7e430aa5c424e15380c5a01.

--- client/core/Menu.jsx
+++ client/core/Menu.jsx
@@ -35,6 +35,9 @@ const Menu = withRouter(({history}) => (
             <HomeIcon/>
           </IconButton>
         </Link>
+        <Link to="/users">
+          <Button style={isActive(history, "/users")}>Users</Button>
+        </Link>
 
         <Link to="/users">
         <Button style={isActive(history, "/users")}>Readers</Button>
